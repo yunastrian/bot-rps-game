a4adcd1e9b7a1ab50f9dcb89b0d10af7e79306fb
Kurniandha Sukma Yunastrian <37417623+yunastrian@users.noreply.github.com>
2021-03-20 22:13:47 +0700

    Create bot_rps.py

---
+++ bot_rps.py
@@ -0,0 +1,14 @@
+moves = ['paper', 'rock', 'scissor']
+player_move = ''
+
+while player_move not in moves :
+  player_move = input('Choose paper, rock, scissor: ')
+
+bot_move = 'rock'
+if (player_move == 'paper') :
+  bot_move = 'scissor'
+elif (player_move == 'rock') :
+  bot_move = 'paper'
+
+print('\nbot has choosen:', bot_move)
+print('You Lose! Try Again!')
