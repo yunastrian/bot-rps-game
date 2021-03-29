37cd5905ae58698bcfa85394213a937eb943dfb9
Muhammad Alif Arifin <m.alif.arifin@gmail.com>
2021-03-29 22:34:19 +0700

    Add support for other language (#2)
    * add .gitignore
    
    * add variables to add the possibility to support other language
    
    * update the main code
    
    * add option to choose language
    
    * Update readme and add requirements.txt
    
    * fix wording of usage message
    
    * bug fix wrong translation

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # bot-rps-game
 I put my heart, energy and mind into developing this bot. After that time, finally I have succeeded in developing a bot that can always win in rock, paper, scissor games. Do you dare to challenge my bot? 
 
+# How to Use
+You can use `h` flag to see what's the options are.
+```
+> python3 bot_rps.py -h
+```
+
 # Demo
 ```
 > python3 bot_rps.py
@@ -11,7 +17,20 @@ You Lose! Try Again!
 ```
 
 # Requirement
-Python 3+
+- Python 3+
+
+Run this command to install the dependency
+```
+pip3 install -r requirements.txt
+```
+
+
+# Supported Language
+Supported languages till today:
+| code | language   |
+|------|------------|
+| en   | English    |
+| id   | Indonesian |
 
 # Contributing
 If you have a better idea, I'd be happy to welcome your feedback 
--- bot_rps.py
+++ bot_rps.py
@@ -1,14 +1,67 @@
-moves = ['paper', 'rock', 'scissor']
+import getopt
+import sys
+from typing import Dict
+import yaml
+
+moves_lang2en: Dict = dict()
+moves_en2lang: Dict = dict()
+
+# default value
+variables_filename = 'variables.yml'
+lang = 'en'
+
+variables_file = open(variables_filename, 'r')
+variable_dict: Dict = yaml.safe_load(variables_file)
+variables_file.close()
+
+supported_languages = variable_dict.keys()
+
+argv = sys.argv[1:]
+usage_message = (f"Usage: python {__file__} [OPTIONS]\n"
+                 "\n"
+                 "Options:\n"
+                 "-l, --language     Select the language of the game.\n"
+                 "                   The default is english.\n")
+
+try:
+    opts, args = getopt.getopt(argv, "hl:",
+                               ["help", "language ="])
+except Exception:
+    print('Invalid option\n')
+    print(usage_message)
+    sys.exit(2)
+
+for opt, arg in opts:
+    if opt in ("-h", "--help"):
+        print(usage_message)
+        sys.exit()
+    elif opt in ("-l", "--language"):
+        if arg not in supported_languages:
+            print(f"Invalid arguments for language: {arg}")
+            print(f'Supported languages: {", ".join(supported_languages)}')
+            sys.exit(3)
+        lang = arg
+
+variables: Dict = variable_dict[lang]
+
+for en, lang in variables['moves'].items():
+    moves_en2lang[en] = lang
+    moves_lang2en[lang] = en
+
 player_move = ''
+while player_move not in moves_lang2en:
+    input_message = variables['messages']['input'] \
+        .format(moves=", ".join(moves_lang2en.keys()))
+    player_move = input(input_message).lower().strip()
 
-while player_move not in moves :
-  player_move = input('Choose paper, rock, scissor: ').lower()
-
+player_move = moves_lang2en[player_move]
 bot_move = 'rock'
-if (player_move == 'paper') :
-  bot_move = 'scissor'
-elif (player_move == 'rock') :
-  bot_move = 'paper'
+if (player_move == 'paper'):
+    bot_move = 'scissor'
+elif (player_move == 'rock'):
+    bot_move = 'paper'
+bot_move = moves_en2lang[bot_move]
 
-print('\nbot has choosen:', bot_move)
-print('You Lose! Try Again!')
+print()
+print(variables['messages']['bot_pick'].format(bot_move=bot_move))
+print(variables['messages']['defeat'])
