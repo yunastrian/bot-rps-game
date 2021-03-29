a94a3deac8eba25a60f206b830789cdf11d2cb7d
Muhammad Alif Arifin <m.alif.arifin@gmail.com>
2021-03-29 21:31:15 +0700

    update the main code

--- bot_rps.py
+++ bot_rps.py
@@ -1,14 +1,37 @@
-moves = ['paper', 'rock', 'scissor']
+from typing import Dict
+import yaml
+
+moves_lang2en: Dict = dict()
+moves_en2lang: Dict = dict()
+
+# default value
+variables_filename = 'variables.yml'
+lang = 'en'
+
+variables_file = open(variables_filename, 'r')
+variable_dict = yaml.safe_load(variables_file)
+variables_file.close()
+
+variables: Dict = variable_dict[lang]
+
+for en, lang in variables['moves'].items():
+    moves_en2lang[en] = lang
+    moves_lang2en[lang] = en
+
 player_move = ''
+while player_move not in moves_lang2en:
+    input_message = variables['messages']['input'] \
+        .format(moves=", ".join(moves_lang2en.keys()))
+    player_move = input(input_message).lower().strip()
 
-while player_move not in moves :
-  player_move = input('Choose paper, rock, scissor: ').lower()
-
+player_move = moves_lang2en[player_move]
 bot_move = 'rock'
-if (player_move == 'paper') :
-  bot_move = 'scissor'
-elif (player_move == 'rock') :
-  bot_move = 'paper'
+if (player_move == 'paper'):
+    bot_move = 'scissor'
+elif (player_move == 'rock'):
+    bot_move = 'paper'
+bot_move = moves_en2lang[bot_move]
 
-print('\nbot has choosen:', bot_move)
-print('You Lose! Try Again!')
+print()
+print(variables['messages']['bot_pick'].format(bot_move=bot_move))
+print(variables['messages']['defeat'])
